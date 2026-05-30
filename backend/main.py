04bb631f4b9daefe064b08ea3366e932206b78db
zain-anwer <zain.anwer192005@gmail.com>
2026-05-30 20:49:12 +0500

    Added github workflow files for syncing

--- backend/main.py
+++ backend/main.py
@@ -135,4 +135,4 @@ def query_results(body: QueryRequest):
     return QueryResponse(query=post, results=results)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7860, reload=True)
